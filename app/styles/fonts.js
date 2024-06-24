211cba0e0610a12f033593512c1fa77ecbb15738
keiner <keinermendoza@gmail.com>
2024-06-23 22:49:28 -0400

    pre deploy

--- app/styles/fonts.js
+++ app/styles/fonts.js
@@ -2,10 +2,13 @@ import { Inika, Fredericka_the_Great } from 'next/font/google';
  
 export const inika = Inika({
     weight: ['400'],
-    subsets: ['latin']
+    subsets: ['latin'],
+    variable: '--font-inika'
 });
  
 export const frede = Fredericka_the_Great({
   weight: ['400'],
   subsets: ['latin'],
+  variable: '--font-frede'
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // sans: ['var(--font-inika)'],
+      },
       colors: {
         "c-blue": {
           primary: '#0C0C81',
